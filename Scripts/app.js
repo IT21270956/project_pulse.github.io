8e4db4f7624dfc9bffea7c78162d2c3ea7de8fc2
hiru1807 <kaushalyamunagama2001@gmail.com>
2025-06-01 19:07:42 +0530

    Final

---
+++ Scripts/app.js
@@ -0,0 +1,21 @@
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".nav-links li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.offsetHeight;
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active-link");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active-link");
+    }
+  });
+});
